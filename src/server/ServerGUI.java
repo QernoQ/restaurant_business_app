ea45ac623c06fc7d693b4783bdf9471cdd4518f7
P.B. <patrykbochenski3@gmail.com>
2025-05-21 19:23:03 +0200

    feat: add server gui

--- src/server/ServerGUI.java
+++ src/server/ServerGUI.java
@@ -1,4 +1,27 @@
-﻿package server;
+package server;
 
-public class ServerGUI {
-}
+import javax.swing.*;
+import java.awt.*;
+
+public class ServerGUI extends JFrame {
+    private JTextArea serverArea;
+    public ServerGUI(String title) {
+        super(title);
+        init();
+    }
+    public void init() {
+        setLocationRelativeTo(null);
+        setSize(600, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        serverArea = new JTextArea();
+        serverArea.setEditable(false);
+        serverArea.setFocusable(false);
+        add(serverArea, BorderLayout.CENTER);
+        JScrollPane js = new JScrollPane(serverArea);
+        add(js, BorderLayout.CENTER);
+        setVisible(true);
+
+    }
+
+    }
